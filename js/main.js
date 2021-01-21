cebfc620c087dde42b48a6ae5ca5ba4f5aed2034
nishiguchi-michael <43350657+nishiguchi-michael@users.noreply.github.com>
2021-01-21 11:25:43 -0500

    captcha and formspar

--- js/main.js
+++ js/main.js
@@ -1,66 +1,40 @@
-// Get the modal
-//var modal = document.getElementById("myModal");
+$(document).ready(function () {
 
-// Get the button that opens the modal
+  //open person modal
+  $('figure.person').click(function (event) {
+    toggleModal(event);
+  });
+  $('.definition').click(function(event) {
+    toggleModal(event);
+  });
 
-$(document).ready(function() {
-  $('figure.person').click(function(event) {
+  function toggleModal(myEvent) {
+    var modal = "#" + myEvent.currentTarget.id + "-modal";
 
-    var modal = "#" + event.currentTarget.id + "-modal";
     var span = $("close");
-
-    console.log(modal);
-
-    //display modal
     $(modal).css("display", "block");
 
     //close modal with click outside modal
-    $(modal).click(function() {
+    $(modal).click(function () {
       $(modal).css("display", "none");
+
     });
+  }
+
+  // expanded progress bar info 
+  $('.progress').click(function () {
+
+    if ($(this).next().css("display") == "none") {
+      $(this).next().css("display", "block");
+    }
+    else {
+      $(this).next().css("display", "none");
+    }
+
   });
-
-  $('section.pipeline').click(function(event) {
-console.log("ehh");
-    //display modal
-    $(".nm108").css("display", "block");
-
-    //close modal with click outside modal
-    $(modal).click(function() {
-      $(modal).css("display", "none");
-    });
-  });
-
-
 });
 
-var btn = document.getElementById("jeffrey");
+//  http://api.edamam.com/auto-complete?q=rou&limit=10&app_id=$90aa6a3e&app_key=$24f01456634cdf030a22c0e6bb73f0a3
 
-//modal is name + modal
-//var modal = btn+"-modal";
 
-//console.log(modal);
-
-/*
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-*/
 
