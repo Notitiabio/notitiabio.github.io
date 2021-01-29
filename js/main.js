407631ef1ee143abe0f5d1433731eac7eece4f56
nishiguchi-michael <43350657+nishiguchi-michael@users.noreply.github.com>
2021-01-29 10:48:58 -0500

    stoy video added

--- js/main.js
+++ js/main.js
@@ -71,7 +71,7 @@ const html = document.documentElement;
 const canvas = document.getElementById("lifestyle-video");
 const context = canvas.getContext("2d");
 
-const frameCount = 305;
+const frameCount = 305  ;
 
 const currentFrame = index => (`images/video/frame-${index.toString().padStart(3, '0')}.jpg`);
 
@@ -80,7 +80,6 @@ const preloadImages = () => {
   for (let i = 1; i < frameCount; i++) {
     const img = new Image();
     img.src = currentFrame(i);
-    console.log("am i ever here?");
   }
 };
 
@@ -102,17 +101,23 @@ const updateImage = index => {
 
 var breakpoint = false;
 
+var breakpoint1 = 2000;
+var breakpoint2 = 50000;
+
 $(window).scroll(function() {
-  
   //if lower than hero, then scroll video
-  if ($(this).scrollTop() > 500 && !breakpoint ) {
-    $(".movie-container").css("position", "fixed");
+  if ($(this).scrollTop() > breakpoint1 && $(this).scrollTop() < breakpoint2 ) {
+    $(".movie-container canvas").css("position", "fixed");
      changeFrame();
   }
   // if above the hero
-  if ($(this).scrollTop() < 400 && breakpoint ) {
-    $(".movie-container").css("position", "relative");
+  if ($(this).scrollTop() < breakpoint1) { // && breakpoint
+    $(".movie-container canvas").css("position", "static");
+
      doOtherStuff();
+  }
+  if($(this).scrollTop() > breakpoint2) {
+    $(".movie-container canvas").css("position", "static");
   }
 
   
@@ -121,24 +126,28 @@ $(window).scroll(function() {
 });
 
 function changeFrame() {
+  let videoHeight = 9460;
+
   const scrollTop = html.scrollTop;
+  //const maxScrollTop = html.scrollHeight - window.innerHeight;
   
-  const maxScrollTop = html.scrollHeight - window.innerHeight;
   const scrollFraction = scrollTop / maxScrollTop;
   const frameIndex = Math.min(
     frameCount - 1,
     Math.ceil(scrollFraction * frameCount)
     );
+
+console.log("HTML scroll heght: " + html.scrollHeight);
+console.log("HTML scroll top: " + html.scrollTop);
+
   requestAnimationFrame(() => updateImage(frameIndex + 1))
 }
 
 function doStuff() {
-  breakpoint = true;
   console.log('we passed the breakpoint, do stuff');
 }
 function doOtherStuff() {
   breakpoint = false;
-  console.log('were above the breakpoint, do other stuff');
 }
 
 
